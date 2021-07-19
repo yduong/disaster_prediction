566debef0dcb4f33dd0a3407396b1a3dcf4e6daf
I347637 <denise.nguyen@sap.com>
2021-07-19 23:13:16 +0800

    commit code for project

---
+++ script/model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+## TODO: Complete this classifier
+class TextClassifier(nn.Module):
+    
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        """
+        Initialize the model by settingg up the various layers.
+        """
+        super(TextClassifier, self).__init__()
+
+        # defining 2 linear layers
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.drop = nn.Dropout(0.3)
+        # sigmoid layer
+        self.sig = nn.Sigmoid()
+    
+    ## TODO: Define the feedforward behavior of the network
+    def forward(self, x):
+        '''Feedforward behavior of the net.
+           :param x: A batch of input features
+           :return: A single, sigmoid activated value
+         '''
+        out = F.relu(self.fc1(x)) # activation on hidden layer
+        out = self.drop(out)
+        out = self.fc2(out)
+        return self.sig(out) # returning class score
